34e4df97a4a5760c90a00c86ff90302f24735463
Amrit27kaur <41029518+Amrit27kaur@users.noreply.github.com>
2019-04-03 18:04:55 -0700

    fargment

--- Android_Projects/FragmentApril3/app/src/main/java/com/example/fragmentapril3/MainActivity.java
+++ Android_Projects/FragmentApril3/app/src/main/java/com/example/fragmentapril3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fragmentapril3;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn1(View v)
+    {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.replace(R.id.fragmentLayout , new listFargment());
+
+        ft.commit();
+    }
+
+
+    public void btn2(View v)
+    {
+   FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+
+   ft1.replace(R.id.fragmentLayout , new addFragment());
+
+   ft1.commit();
     }
 }
